ac163d26cc3a64b43c7b1617a3460f93e84bde4e
Ryan Seys <ryan@ryanseys.com>
2014-09-20 21:46:56 -0400

    Initial Commit

---
+++ keyboard-cat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  keyboard-cat
+//
+//  Created by Ryan Seys on 2014-09-20.
+//  Copyright (c) 2014 Ryan Seys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ keyboard-catTests/keyboard_catTests.swift
@@ -0,0 +1,36 @@
+//
+//  keyboard_catTests.swift
+//  keyboard-catTests
+//
+//  Created by Ryan Seys on 2014-09-20.
+//  Copyright (c) 2014 Ryan Seys. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class keyboard_catTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
