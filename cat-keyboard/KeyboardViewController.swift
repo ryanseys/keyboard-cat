adda668d5770e3572a2a149432e30506602a80ac
Ryan Seys <ryan@ryanseys.com>
2015-04-19 23:45:47 -0400

    Update for newest iOS

--- cat-keyboard/KeyboardViewController.swift
+++ cat-keyboard/KeyboardViewController.swift
@@ -22,17 +22,17 @@ class KeyboardViewController: UIInputViewController {
         super.viewDidLoad()
         
         var xibViews = NSBundle.mainBundle().loadNibNamed("cat-keyboard", owner: self, options: nil)
-        self.mainView = xibViews[0] as UIView
+        self.mainView = xibViews[0] as! UIView
         self.view.addSubview(mainView)
         
-        for v in self.mainView.subviews as [UIButton] {
+        for v in self.mainView.subviews as! [UIButton] {
             v.addTarget(self, action: "btnPressed:", forControlEvents: .TouchUpInside)
         }
     }
     
     func btnPressed(sender: AnyObject) {
-        var btn = sender as UIButton
-        let proxy = self.textDocumentProxy as UIKeyInput
+        var btn = sender as! UIButton
+        let proxy = self.textDocumentProxy as! UIKeyInput
         
         switch (btn.tag) {
             case 1:
@@ -44,8 +44,8 @@ class KeyboardViewController: UIInputViewController {
             case 5:
                 // copy keyboard cat
                 let imageStringURL = NSBundle.mainBundle().pathForResource("keyboardcat", ofType: "gif")
-                var data = NSData(contentsOfURL: NSURL(fileURLWithPath: imageStringURL!));
-                UIPasteboard.generalPasteboard().setData(data, forPasteboardType: "com.compuserve.gif")
+                var data = NSData(contentsOfURL: NSURL(fileURLWithPath: imageStringURL!)!);
+                UIPasteboard.generalPasteboard().setData(data!, forPasteboardType: "com.compuserve.gif")
             default:
                 return
         }
@@ -64,7 +64,7 @@ class KeyboardViewController: UIInputViewController {
         // The app has just changed the document's contents, the document context has been updated.
     
         var textColor: UIColor
-        var proxy = self.textDocumentProxy as UITextDocumentProxy
+        var proxy = self.textDocumentProxy as! UITextDocumentProxy
         if proxy.keyboardAppearance == UIKeyboardAppearance.Dark {
             textColor = UIColor.whiteColor()
         } else {
